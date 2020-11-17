a91c591b1d8f1d7e080bf964ff9b0b333fcb0987
keydian <49322025+keydian@users.noreply.github.com>
2020-11-17 21:53:45 +0000

    Add files via upload

--- mambusbot.js
+++ mambusbot.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 const client = new Discord.Client();
-client.data = require("./data.json");
 client.login(process.env.BOT_TOKEN).then(console.log);
 client.on('ready', readyDiscord);
 
 //variables and counters
-let penis = client.data["penis"];
-let macroCounter = client.data["macroCounter"];
-let microCounter = client.data["microCounter"];
+let penis = "8D";
+let macroCounter = 0;
+let microCounter = 0;
+let mambusArray = [
+    "https://ibb.co/xDks7yL", //stores all mambusStranhus.png
+    "https://ibb.co/Qp7Mr0z",
+    "https://ibb.co/xfrc1Vw",
+    "https://ibb.co/PQ119QT",
+    "https://ibb.co/nfYW80t",
+    "https://ibb.co/TqWkkBM",
+    "https://ibb.co/yqyzSJs",
+    "https://ibb.co/PgX7rNB",
+    "https://ibb.co/TvK3743",
+    "https://ibb.co/V37yY4D",
+    "https://ibb.co/CzrXH0S",
+    "https://ibb.co/zbnLcq8"
+];
 
 
 function readyDiscord(){
@@ -25,6 +37,8 @@ function readyDiscord(){
 
 client.on('message', message => {
     if(!message.author.bot){
+
+        //REACTIVE EVENTS
         if (message.content.includes("birra")){ //birra trigger
             message.react("🇫"); //react 'F' emoji
             message.react("🇩"); //react 'D' emoji
@@ -38,21 +52,7 @@ client.on('message', message => {
             message.reply("Toma um copo de João Leite: https://ibb.co/tpX6SDt");
         }
         if(message.content.includes("mambus")){
-            let rng = Math.ceil(Math.random() * 11); //11 = array.length-1
-            let mambusArray = [
-                "https://ibb.co/xDks7yL", //stores all mambusStranhus.png
-                "https://ibb.co/Qp7Mr0z",
-                "https://ibb.co/xfrc1Vw",
-                "https://ibb.co/PQ119QT",
-                "https://ibb.co/nfYW80t",
-                "https://ibb.co/TqWkkBM",
-                "https://ibb.co/yqyzSJs",
-                "https://ibb.co/PgX7rNB",
-                "https://ibb.co/TvK3743",
-                "https://ibb.co/V37yY4D",
-                "https://ibb.co/CzrXH0S",
-                "https://ibb.co/zbnLcq8"
-            ]
+            let rng = Math.round(Math.random() * 11); //11 = array.length-1
             message.reply(mambusArray[rng]).then(msg => {
                     msg.react("MAMBUS:775834254939652096"); //reacts with mambus
                 }
@@ -67,35 +67,40 @@ client.on('message', message => {
         if (message.content.includes("macropenis") || message.content.includes("macropénis")){
             penis = insert(penis, 1, "="); //increases penis size
             macroCounter++;
-            client.data["penis"] = penis;
-            client.data["macroCounter"]++;
-            fs.writeFile("./data.json", JSON.stringify(client.data, null, 2), err =>{
-                if (err) console.log(err);
-            })
         }
         if(message.content.includes("micropenis") || message.content.includes("micropénis")){
             if(penis.length>2){
                 penis = removeAt(2, penis);
                 microCounter++;
-                client.data["penis"] = penis;//decreases penis size
-                client.data["microCounter"]++;
-                fs.writeFile("./data.json", JSON.stringify(client.data, null, 2), err =>{
-                    if (err) console.log(err);
-                })
             }
             else{
                 message.reply("Olha lá, o teu micropénis n consegue ficar mais pequeno. Não tens vergonha?");
             }
         }
+
+        //MEME COMMANDS
         if(message.content.toString() === "!penis"){ //command to print penis and penis stats
             message.reply("\n"+ penis + "\n" +
-                "Número de vezes que se invocou o macropénis: "+macroCounter+"\n"+
-                "Número de vezes que se invocou o micropénis: "+microCounter);
+                "Número de vezes que se invocou o macropénis desde que o bot levou update: "+macroCounter+"\n"+
+                "Número de vezes que se invocou o micropénis desde que o bot levou update: "+microCounter);
         }
+        if(message.content.toString() === "!ping"){
+            let usersArray = client.users.cache.array();
+            let triggerChance = Math.random();
+            if(triggerChance < 1){
+                let randomUser = Math.round(Math.random() * usersArray.length);
+                message.channel.send(randomUser + "agradece ao "+ message.author+" btw.");
+            }
+        }
+
+        //ACTUALLY USEFUL COMMANDS
         if(message.content.toString() === "!github"){
             message.reply("https://github.com/keydian/botmambus.js").then(msg =>{
                 msg.react("MAMBUS:775834254939652096");
             })
+        }
+        if(message.content.toString() === "!githubRC"){
+            message.reply("https://github.com/jlegatheaux/RC2020-assignments");
         }
     }
 })
