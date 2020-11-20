5beaed13eadfbdf601288744c06fc0109406deb1
keydian <49322025+keydian@users.noreply.github.com>
2020-11-20 02:18:40 +0000

    Add files via upload

--- mambusbot.js
+++ mambusbot.js
@@ -1,6 +1,6 @@
-const Discord = require('discord.js');
+const {Client, Attachment} = require('discord.js');
 const GoogleImages = require('google-images');
-const client = new Discord.Client();
+const client = new Client.Client();
 const grabGImages = new GoogleImages("", "");
 client.login(process.env.BOT_TOKEN).then(console.log);
 client.on('ready', readyDiscord);
