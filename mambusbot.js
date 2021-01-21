2f2a38cad6587c5ffb3115b1b52c0a41f5095727
keydian <49322025+keydian@users.noreply.github.com>
2021-01-21 15:01:40 +0000

    Deleted MongoDB Implementation
    It was giving more problems than solving them.

--- mambusbot.js
+++ mambusbot.js
@@ -1,37 +1,15 @@
 const {Client} = require('discord.js');
-const mongo = require('mongodb').MongoClient;
 const client = new Client;
-client.login(process.env.BOT_TOKEN);
+client.login("Nzc3ODk4MzUzNzA1ODc3NTE0.X7KITA.sDaesgR1clCNwIi_845IytmCP7s");
 client.on('ready', readyDiscord);
-const url = encodeURI(process.env.MONGODB_URI);
-let database;
 
-mongo.connect(url, {useUnifiedTopology: true}, function (err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    database = db.db("botMambusJS");
-    let firstRun = database.collection("boolean").find({name: "firstRun"}).limit(1).next();
-    if(firstRun.toString().includes("value: true")){
-        const server = client.guilds.cache.get("398569110885367817")
-        server.members.cache.forEach(member => {
-                let userData = {userID: member.id, userName: member.user.username, balance: 0};
-                database.collection("users").insertOne(userData);
-                console.log("User added to database.")
-            }
-        )
-        database.collection("boolean").updateOne({name: firstRun},{$set:{'value':'false'}});
-    }
-
-
-
-});
 
 function readyDiscord(){
     client.user.setPresence({
         activity: {
             name: 'Analisando Mambus Stranhus em projetos...'
         },
-    }).then(console.log);
+    }).then(console.log("Bot connected!"));
     client.channels.cache.find(channel => channel.name === "bot-spam").send("Bot logged in.").
     then(msg =>{
         msg.react("MAMBUS:775834254939652096"); //react com mambus emoji
